9b2784464de715b3bb036883bda8262f8bb9f107
Austin Reuschle <areuschle@roblox.local>
2019-09-17 11:28:47 -0700

    added gameplay input types

--- src/client/gameModules/InputHandler/createInputSpec.lua
+++ src/client/gameModules/InputHandler/createInputSpec.lua
@@ -1,42 +1,99 @@
 return function()
     return {
+
+        -- game actions
         attack = {
             inputs = {
                 {
                     type = Enum.UserInputType.MouseButton1,
                     keyCode = nil, -- respond to all inputs of the type
-                    passThrough = true, -- dont block
                     state = Enum.UserInputState.Begin, -- trigger on this state
                 },
                 {
                     type = Enum.UserInputType.Keyboard,
                     keyCode = Enum.KeyCode.F,
-                    passThrough = true,
                     state = Enum.UserInputState.Begin,
                 },
                 {
                     type = Enum.UserInputType.Gamepad1,
-                    keyCode = Enum.KeyCode.ButtonR1,
-                    passThrough = true,
+                    keyCode = Enum.KeyCode.ButtonR2,
                     state = Enum.UserInputState.Begin,
                 },
-            }
+            },
         },
         roll = {
             inputs = {
                 {
                     type = Enum.UserInputType.Keyboard,
                     keyCode = Enum.KeyCode.LeftShift,
-                    passThrough = true,
                     state = Enum.UserInputState.Begin,
                 },
                 {
                     type = Enum.UserInputType.Gamepad1,
-                    keyCode = Enum.KeyCode.ButtonL1,
-                    passThrough = true,
+                    keyCode = Enum.KeyCode.ButtonL2,
                     state = Enum.UserInputState.Begin,
                 },
-            }
-        }
+            },
+        },
+        interact = {
+            inputs = {
+                {
+                    type = Enum.UserInputType.Keyboard,
+                    keyCode = Enum.KeyCode.E,
+                    state = Enum.UserInputState.Begin,
+                },
+                {
+                    type = Enum.UserInputType.Gamepad1,
+                    keyCode = Enum.KeyCode.ButtonX,
+                    state = Enum.UserInputState.Begin,
+                },
+            },
+        },
+        special = {
+            inputs = {
+                {
+                    type = Enum.UserInputType.Keyboard,
+                    keyCode = Enum.KeyCode.R,
+                    state = Enum.UserInputState.Begin,
+                },
+                {
+                    type = Enum.UserInputType.Gamepad1,
+                    keyCode = Enum.KeyCode.ButtonY,
+                    state = Enum.UserInputState.Begin,
+                },
+            },
+        },
+        trinket = {
+            inputs = {
+                {
+                    type = Enum.UserInputType.Keyboard,
+                    keyCode = Enum.KeyCode.Q,
+                    state = Enum.UserInputState.Begin,
+                },
+                {
+                    type = Enum.UserInputType.Gamepad1,
+                    keyCode = Enum.KeyCode.DPadDown,
+                    state = Enum.UserInputState.Begin,
+                },
+            },
+        },
+        nextWeapon = {
+            inputs = {
+                {
+                    type = Enum.UserInputType.Gamepad1,
+                    keyCode = Enum.KeyCode.ButtonR1,
+                    state = Enum.UserInputState.Begin,
+                },
+            },
+        },
+        prevWeapon = {
+            inputs = {
+                {
+                    type = Enum.UserInputType.Gamepad1,
+                    keyCode = Enum.KeyCode.ButtonL1,
+                    state = Enum.UserInputState.Begin,
+                },
+            },
+        },
     }
 end
