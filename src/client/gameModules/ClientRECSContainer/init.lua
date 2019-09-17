43830ae6af336438a4ea29e35acd10a6601932d5
Nimblz <areuschle@gmail.com>
2019-09-17 13:48:36 -0700

    renamed client recs container and added a server recs container

---
+++ src/client/gameModules/ClientRECSContainer/init.lua
@@ -0,0 +1,47 @@
+-- TODO: Genericize this module and replace with a common RECSContainer
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local lib = ReplicatedStorage:WaitForChild("lib")
+local recsPlugins = common:WaitForChild("recsplugins")
+
+local RECS = require(lib:WaitForChild("RECS"))
+local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
+
+local RecsComponents = require(common:WaitForChild("RecsComponents"))
+local Systems = require(script.Systems)
+local Steppers = require(script.Steppers)
+
+local createInjectorPlugin = require(recsPlugins:WaitForChild("createInjectorPlugin"))
+
+local ClientRecsContainer = PizzaAlpaca.GameModule:extend("ClientRecsContainer")
+
+function ClientRecsContainer:preInit()
+end
+
+function ClientRecsContainer:init()
+    self.recsCore = RECS.Core.new({
+        RECS.BuiltInPlugins.CollectionService(),
+        RECS.BuiltInPlugins.ComponentChangedEvent,
+        createInjectorPlugin("getClientModule", function(_, name)
+            return self.core:getModule(name)
+        end),
+    })
+
+    -- register all components
+    for _, component in pairs(RecsComponents) do
+        self.recsCore:registerComponent(component)
+    end
+
+    self.recsCore:registerSystems(Systems)
+    self.recsCore:registerSteppers(Steppers)
+
+    self.recsCore:start()
+end
+
+function ClientRecsContainer:postInit()
+end
+
+
+return ClientRecsContainer
