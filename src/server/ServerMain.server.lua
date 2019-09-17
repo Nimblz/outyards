c1d0bc6bc29f231961017a2f28caf984baeeb73f
Nimblz <areuschle@gmail.com>
2019-09-17 13:44:43 -0700

    created server entry point

---
+++ src/server/ServerMain.server.lua
@@ -0,0 +1,23 @@
+print("Starting outyards server.")
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local lib = ReplicatedStorage:WaitForChild("lib")
+local common = ReplicatedStorage:WaitForChild("common")
+local server = script.Parent
+
+local sidedModules = server:WaitForChild("gameModules")
+local commonModules = common:WaitForChild("gameModules")
+
+local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
+
+-- create PizzaAlpaca core instance
+local core = PizzaAlpaca.GameCore.new()
+core._debugPrints = true
+
+-- load sided and common modules
+core:registerChildrenAsModules(commonModules)
+core:registerChildrenAsModules(sidedModules)
+
+-- start the core
+core:load()
