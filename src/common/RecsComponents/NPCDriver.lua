6a0bf62b21ee562f04506f4ac5eea0e2b97e742f
Nimblz <areuschle1@gmail.com>
2019-09-18 15:23:19 -0700

    finished driver system

--- src/common/RecsComponents/NPCDriver.lua
+++ src/common/RecsComponents/NPCDriver.lua
@@ -1,3 +1,5 @@
+-- REMEMBER: YOU GOTTA USE component:updateProperty(key,value) TO TRIGGER CHANGED EVENT!
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage:WaitForChild("lib")
@@ -10,7 +12,8 @@ return RECS.defineComponent({
         return {
             gravityWeight = 1,
             targetVelocity = Vector3.new(0,0,0),
-            targetDirection = Vector3.new(0,0,0),
+            targetDirection = Vector3.new(0,0,-1),
+            maxMoveForce = Vector3.new(1000,0,1000),
         }
     end,
 })
