0451901676f3c13a89ea86d2cdf97f9871465e9e
Austin Reuschle <areuschle@roblox.local>
2019-09-18 15:23:19 -0700

    finished driver system

--- src/server/gameModules/ServerRECSContainer/Systems/NPCDriverSystem.lua
+++ src/server/gameModules/ServerRECSContainer/Systems/NPCDriverSystem.lua
@@ -12,25 +12,66 @@ local RecsComponents = require(common:WaitForChild("RecsComponents"))
 
 local NPCDriverSystem = RECS.System:extend("NPCDriverSystem")
 
-function NPCDriverSystem:init()
-    -- on component add create a bodyvelocity bodyforce and bodygyro
-    self.core:getComponentAddedSignal(RecsComponents.NPCDriver):connect(function(instance,component)
-        local bodyVelocity = Instance.new("BodyVelocity")
-        local bodyForce = Instance.new("BodyForce")
-        local bodyGyro = Instance.new("BodyGyro")
+function NPCDriverSystem:onComponentChange(instance, component)
+    local bodyVelocity = instance:FindFirstChild("BodyVelocity")
+    local bodyForce = instance:FindFirstChild("BodyForce")
+    local bodyGyro = instance:FindFirstChild("BodyGyro")
 
-        bodyVelocity.Parent = instance
-        bodyForce.Parent = instance
-        bodyGyro.Parent = instance
+    if not bodyVelocity then return end
+    if not bodyForce then return end
+    if not bodyGyro then return end
+
+    bodyVelocity.MaxForce = component.maxMoveForce
+    bodyVelocity.Velocity = component.targetVelocity
+    bodyForce.Force = Vector3.new(0, instance:GetMass() * workspace.Gravity * (1-component.gravityWeight), 0)
+    bodyGyro.MaxTorque = Vector3.new(10000,10000,10000)
+    bodyGyro.CFrame = CFrame.new(Vector3.new(0,0,0),component.targetDirection)
+end
+
+function NPCDriverSystem:onComponentAdded(instance, component)
+    -- on component add create a bodyvelocity bodyforce and bodygyro
+    local bodyVelocity = Instance.new("BodyVelocity")
+    local bodyForce = Instance.new("BodyForce")
+    local bodyGyro = Instance.new("BodyGyro")
+
+    component.changed:connect(function(prop, new, old)
+        self:onComponentChange(instance, component)
+    end)
+
+    bodyVelocity.Parent = instance
+    bodyForce.Parent = instance
+    bodyGyro.Parent = instance
+
+    self:onComponentChange(instance,component)
+end
+
+function NPCDriverSystem:onComponentRemoving(instance,component)
+    local bodyVelocity = instance:FindFirstChild("BodyVelocity")
+    local bodyForce = instance:FindFirstChild("BodyForce")
+    local bodyGyro = instance:FindFirstChild("BodyGyro")
+
+    if bodyVelocity then
+        bodyVelocity:Destroy()
+    end
+    if bodyForce then
+        bodyForce:Destroy()
+    end
+    if bodyGyro then
+        bodyGyro:Destroy()
+    end
+end
+
+function NPCDriverSystem:init()
+
+    for instance,component in self.core:components(RecsComponents.NPCDriver) do
+        self:onComponentAdded(instance, component)
+    end
+
+    self.core:getComponentAddedSignal(RecsComponents.NPCDriver):connect(function(instance,component)
+        self:onComponentAdded(instance, component)
     end)
     self.core:getComponentRemovingSignal(RecsComponents.NPCDriver):connect(function(instance,component)
-        local bodyVelocity = instance:FindFirstChild("BodyVelocity")
-        local bodyForce = instance:FindFirstChild("BodyForce")
-        local bodyGyro = instance:FindFirstChild("BodyGyro")
-
-        bodyVelocity:Destroy()
-        bodyForce:Destroy()
-        bodyGyro:Destroy()
+        self:onComponentRemoving(instance, component)
     end)
 end
 
