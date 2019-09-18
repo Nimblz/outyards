957ad0d3523c7df946961465dbcd14b1474bd0c5
Nimblz <areuschle@gmail.com>
2019-09-18 14:10:05 -0700

    simplified

--- src/common/util/compileSubmodulesToArray.lua
+++ src/common/util/compileSubmodulesToArray.lua
@@ -1,12 +1,12 @@
-return function(parent, recursive)
-    local compiled = {}
-    local children = (recursive and parent:GetDescendants()) or parent:GetChildren()
+local util = script.Parent
+local requireSubmodules = require(util:WaitForChild("requireSubmodules"))
 
-    for _,moduleScript in pairs(children) do
-        if moduleScript:IsA("ModuleScript") then
-            local required = require(moduleScript)
-            table.insert(compiled, required)
-        end
+return function(parent, recursive)
+    local unordered = requireSubmodules(parent,recursive)
+    local compiled = {}
+
+    for _, module in pairs(unordered) do
+        table.insert(compiled, module)
     end
 
     return compiled
