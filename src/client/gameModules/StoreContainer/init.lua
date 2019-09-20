cb699e640d05f750c2d5c1f9fae66f089a1e8ac2
Nimblz <areuschle1@gmail.com>
2019-09-20 14:43:24 -0700

    setup client store

---
+++ src/client/gameModules/StoreContainer/init.lua
@@ -0,0 +1,56 @@
+-- contains the games store and handles action replication
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local src = script.Parent.Parent
+local lib = ReplicatedStorage:WaitForChild("lib")
+local event = ReplicatedStorage:WaitForChild("event")
+
+local Rodux = require(lib:WaitForChild("Rodux"))
+local Signal = require(lib:WaitForChild("Signal"))
+local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
+local Promise = require(lib:WaitForChild("Promise"))
+
+local reducer = require(src:WaitForChild("clientReducer"))
+
+local eReplicateAction = event:WaitForChild("eReplicateAction")
+
+local StoreContainer = PizzaAlpaca.GameModule:extend("StoreContainer")
+
+function StoreContainer:create()
+    self.storeCreated = Signal.new()
+end
+
+function StoreContainer:getStore()
+    return Promise.async(function(resolve, reject)
+        resolve(self.store or self.storeCreated:wait())
+    end)
+end
+
+function StoreContainer:createStore(initialState)
+    self.logger:log("Server store initialized.")
+    local store = Rodux.Store.new(reducer,initialState, {
+		Rodux.thunkMiddleware,
+    })
+
+    eReplicateAction.OnClientEvent:connect(function(replicatedAction)
+        store:dispatch(replicatedAction)
+    end)
+
+    self.store = store
+
+    self.storeCreated:fire()
+end
+
+function StoreContainer:preInit()
+end
+
+function StoreContainer:init()
+    self.logger = self.core:getModule("Logger"):createLogger(self)
+    self:createStore()
+end
+
+function StoreContainer:postInit()
+end
+
+return StoreContainer
