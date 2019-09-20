ca02eda60d49a3ef7313635e09497cb46e0e6417
Austin Reuschle <areuschle@roblox.local>
2019-09-20 14:26:33 -0700

    implemented selectors

---
+++ src/common/Selectors/getStats.lua
@@ -0,0 +1,8 @@
+local getPlayerState = require(script.Parent.getPlayerState)
+
+return function(state,player)
+    local pstate = getPlayerState(state,player)
+    if pstate then
+        return pstate.stats
+    end
+end
