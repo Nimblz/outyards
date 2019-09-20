6696c9a13f7a0825f70c2c21b1e61db79353a560
Austin Reuschle <areuschle@roblox.local>
2019-09-20 13:36:00 -0700

    roadmapped the basic equipment catagories

---
+++ src/common/EquipmentTypes.lua
@@ -0,0 +1,29 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local by = require(util:WaitForChild("by"))
+
+local catagories = {
+    {
+        id = "weapon",
+    },
+    {
+        id = "armor",
+    },
+    {
+        id = "hat",
+    },
+    {
+        id = "feet",
+    },
+    {
+        id = "weapon",
+    },
+}
+
+return {
+    all = catagories,
+    byId = by("id", catagories),
+}
