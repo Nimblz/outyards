74439b0f4fbaeb02d90612379855cdce4e5142ee
Nimblz <areuschle1@gmail.com>
2019-09-20 15:35:17 -0700

    stats now uses correct reducer

--- src/common/commonReducer/playerState/init.lua
+++ src/common/commonReducer/playerState/init.lua
@@ -2,7 +2,7 @@ local inventory = require(script:WaitForChild("inventory"))
 local equipped = require(script:WaitForChild("equipped"))
 local stats = require(script:WaitForChild("stats"))
 
-return (function(state,action)
+return function(state,action)
     state = state or {}
 
     -- server TODO: break this into two reducers?
@@ -17,6 +17,6 @@ return (function(state,action)
     return {
         inventory = inventory(state.inventory, action),
         equipped = equipped(state.equipped, action),
-        stats = equipped(state.stats, action),
+        stats = stats(state.stats, action),
     }
-end)
+end
