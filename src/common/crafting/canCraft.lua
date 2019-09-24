cf89efeb567827ddde155e9b0ee162558a5bfd07
Nimblz <areuschle1@gmail.com>
2019-09-23 17:08:42 -0700

    item craft util functions

---
+++ src/common/crafting/canCraft.lua
@@ -0,0 +1,22 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+
+local Selectors = require(common:WaitForChild("Selectors"))
+local Items = require(common:WaitForChild("Items"))
+
+local function canCraft(state,player,itemId)
+    local item = Items.byId[itemId]
+    if not item then return false end
+    local recipe = item.recipe
+    if not recipe then return false end
+
+    for id, quantity in pairs(recipe) do
+        if Selectors.getItem(state, player, id) < quantity then
+            return false
+        end
+    end
+    return true
+end
+
+return canCraft
