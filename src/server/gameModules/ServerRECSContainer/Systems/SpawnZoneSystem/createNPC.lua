c1ae506e96f36bc6dfe7f40b42c187e0d8b7fe3d
Nimblz <areuschle@gmail.com>
2019-09-25 09:49:14 -0700

    implemented function to create an npc

---
+++ src/server/gameModules/ServerRECSContainer/Systems/SpawnZoneSystem/createNPC.lua
@@ -0,0 +1,29 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+
+local common = ReplicatedStorage:WaitForChild("common")
+
+local NPCS = require(common:WaitForChild("NPCS"))
+local RecsComponents = require(common:WaitForChild("RecsComponents"))
+
+local errors = {
+    invalidMob = "Invalid NPC type [%s]!"
+}
+
+return function(recsCore, npcType, cframe)
+    local mobDesc = NPCS.byType[npcType]
+    assert(mobDesc, errors.invalidMob:format(npcType))
+
+    local NewNPCPart = Instance.new("Part")
+    NewNPCPart.TopSurface = Enum.SurfaceType.Smooth
+    NewNPCPart.BottomSurface = Enum.SurfaceType.Smooth
+    NewNPCPart.Size = mobDesc.boundingBoxProps.Size
+    NewNPCPart.Color= mobDesc.boundingBoxProps.Color
+    NewNPCPart.CFrame = cframe * CFrame.new(0,(NewNPCPart.Size.Y/2) + 1, 0)
+
+    NewNPCPart.Parent = workspace
+
+    recsCore:addComponent(NewNPCPart,RecsComponents.NPC, {npcType = npcType})
+
+    return NewNPCPart
+end
