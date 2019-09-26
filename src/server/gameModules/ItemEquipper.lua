e691bb830cc405c752584446322988e06b513267
Austin Reuschle <areuschle@roblox.local>
2019-09-26 16:12:36 -0700

    module to equip items

---
+++ src/server/gameModules/ItemEquipper.lua
@@ -0,0 +1,28 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local lib = ReplicatedStorage:WaitForChild("lib")
+local event = ReplicatedStorage:WaitForChild("event")
+local util = common:WaitForChild("util")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
+local RecsComponents = require(common:WaitForChild("RecsComponents"))
+local Thunks = require(common:WaitForChild("Thunks"))
+local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
+
+local ItemEquipper = PizzaAlpaca.GameModule:extend("ItemEquipper")
+
+local eRequestEquip = event:WaitForChild("eRequestEquip")
+
+function ItemEquipper:onStore(store)
+    eRequestEquip.OnServerEvent:connect(function(player, itemId)
+        store:dispatch(Thunks.ITEM_EQUIP(player,itemId))
+    end)
+end
+
+function ItemEquipper:init()
+    local storeContainer = self.core:getModule("StoreContainer")
+    storeContainer:getStore():andThen(function(store) self:onStore(store) end)
+end
+
+return ItemEquipper
