95aeedf36cb8259025c83661ecb4ca07825b3bda
Austin Reuschle <areuschle@roblox.local>
2019-09-26 16:07:07 -0700

    added sprite and stats

--- src/common/Items/equipment/tier2/IronArmor.lua
+++ src/common/Items/equipment/tier2/IronArmor.lua
@@ -5,10 +5,16 @@ return {
     name = "Iron Armor",
     equipmentType = "armor",
     tier = 2,
+    spriteSheet = "armor",
+    spriteCoords = Vector2.new(2,1),
     recipe = {
         leather = 80,
         ingotIron = 50,
         wood = 30,
         ingotCopper = 20,
+    },
+
+    stats = {
+        defense = 75,
     }
 }
