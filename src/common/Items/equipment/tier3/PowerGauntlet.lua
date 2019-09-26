d969cea4314250cb82ebb72d53bcaafd53894429
Nimblz <areuschle1@gmail.com>
2019-09-26 16:07:38 -0700

    added sprite, changed recipe, added stats

--- src/common/Items/equipment/tier3/PowerGauntlet.lua
+++ src/common/Items/equipment/tier3/PowerGauntlet.lua
@@ -5,9 +5,19 @@ return {
     name = "Power Gauntlet",
     equipmentType = "trinket",
     tier = 2,
+    spriteSheet = "armor",
+    spriteCoords = Vector2.new(7,3),
     recipe = {
-        starCloth = 40,
-        stardust = 50,
+        ingotVoid = 30,
+        starCloth = 100,
+        stardust = 150,
         powerShard = 5,
+    },
+
+    stats = {
+        defense = 30,
+        baseDamage = 30,
+        attackRate = 5,
+        autoAttack = true,
     }
 }
