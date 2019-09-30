61144ee15ede110f8f44875a9341acdad55ac3ea
Austin Reuschle <areuschle@roblox.local>
2019-09-30 16:07:07 -0700

    Add getCore promise

--- src/client/gameModules/ClientRECSContainer/init.lua
+++ src/client/gameModules/ClientRECSContainer/init.lua
@@ -8,25 +8,32 @@ local recsPlugins = common:WaitForChild("recsplugins")
 
 local RECS = require(lib:WaitForChild("RECS"))
 local PizzaAlpaca = require(lib:WaitForChild("PizzaAlpaca"))
+local Signal = require(lib:WaitForChild("Signal"))
+local Promise = require(lib:WaitForChild("Promise"))
 
 local RecsComponents = require(common:WaitForChild("RecsComponents"))
 local Systems = require(script.Systems)
 local Steppers = require(script.Steppers)
 
 local createInjectorPlugin = require(recsPlugins:WaitForChild("createInjectorPlugin"))
+local createComponentPropsOverridePlugin = require(recsPlugins:WaitForChild("createComponentPropsOverridePlugin"))
 
-local ClientRecsContainer = PizzaAlpaca.GameModule:extend("ClientRecsContainer")
+local ClientRECSContainer = PizzaAlpaca.GameModule:extend("ClientRECSContainer")
 
-function ClientRecsContainer:preInit()
+function ClientRECSContainer:create()
+    self.recsCoreCreated = Signal.new()
 end
 
-function ClientRecsContainer:init()
+function ClientRECSContainer:onStoreCreated(store)
     self.recsCore = RECS.Core.new({
         RECS.BuiltInPlugins.CollectionService(),
         RECS.BuiltInPlugins.ComponentChangedEvent,
+        createComponentPropsOverridePlugin(),
         createInjectorPlugin("getClientModule", function(_, name)
             return self.core:getModule(name)
         end),
+        createInjectorPlugin("pzCore", self.core),
+        createInjectorPlugin("store", store),
     })
 
     -- register all components
@@ -36,12 +43,29 @@ function ClientRecsContainer:init()
 
     self.recsCore:registerSystems(Systems)
     self.recsCore:registerSteppers(Steppers)
-
-    self.recsCore:start()
 end
 
-function ClientRecsContainer:postInit()
+function ClientRECSContainer:getCore()
+    return Promise.async(function(resolve, reject)
+        resolve(self.recsCore or self.recsCoreCreated:wait())
+    end)
+end
+
+function ClientRECSContainer:preInit()
+end
+
+function ClientRECSContainer:init()
+    local storeContainer = self.core:getModule("StoreContainer")
+    storeContainer:getStore():andThen(function(store)
+        self:onStoreCreated(store)
+
+        self.recsCore:start()
+        self.recsCoreCreated:fire(self.recsCore)
+    end)
+end
+
+function ClientRECSContainer:postInit()
 end
 
 
-return ClientRecsContainer
+return ClientRECSContainer
