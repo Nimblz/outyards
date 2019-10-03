70214af480d314f808b1b0f26859a1003dfdf325
Nimblz <areuschle1@gmail.com>
2019-10-03 14:00:48 -0700

    based on default moves speed

--- src/common/commonReducer/playerState/stats/moveSpeed.lua
+++ src/common/commonReducer/playerState/stats/moveSpeed.lua
@@ -1,5 +1,9 @@
+local StarterPlayer = game:GetService("StarterPlayer")
+
+local DEFAULT_SPEED = StarterPlayer.CharacterWalkSpeed
+
 return function(state, action)
-    state = state or 16
+    state = state or DEFAULT_SPEED
 
     if action.type == "MOVESPEED_SET" then
         return action.moveSpeed
