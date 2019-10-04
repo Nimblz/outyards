dd0ba5d7e3fd70c016c99ed04be3128ec02eee74
Nimblz <areuschle@gmail.com>
2019-10-04 09:40:34 -0700

    Merge props

--- src/common/RecsComponents/AI.lua
+++ src/common/RecsComponents/AI.lua
@@ -1,14 +1,18 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage:WaitForChild("lib")
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
 
 local RECS = require(lib:WaitForChild("RECS"))
 
 return RECS.defineComponent({
     name = "AI",
-    generator = function()
-        return {
+    generator = function(props)
+        return Dictionary.join({
             aiType = "Fighter"
-        }
+        },props)
     end,
 })
--- src/common/RecsComponents/SpawnZone.lua
+++ src/common/RecsComponents/SpawnZone.lua
@@ -1,17 +1,21 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage:WaitForChild("lib")
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
 
 local RECS = require(lib:WaitForChild("RECS"))
 
 return RECS.defineComponent({
     name = "SpawnZone",
-    generator = function()
-        return {
+    generator = function(props)
+        return Dictionary.join({
             spawnables = {},
             spawnRate = 1, -- every n ticks
             counter = 0, -- when counter == rate, cointer gets set to 0 and mob is spawned
             spawnCap = 100,
-        }
+        },props)
     end,
 })
