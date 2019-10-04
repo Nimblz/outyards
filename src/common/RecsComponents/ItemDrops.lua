51435b854fe66fd2fc3727eb5998df0d7d0a6900
Nimblz <areuschle1@gmail.com>
2019-10-04 09:40:34 -0700

    Merge props

--- src/common/RecsComponents/ItemDrops.lua
+++ src/common/RecsComponents/ItemDrops.lua
@@ -1,17 +1,21 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage:WaitForChild("lib")
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
 
 local RECS = require(lib:WaitForChild("RECS"))
 
 return RECS.defineComponent({
     name = "ItemDrops",
-    generator = function()
-        return {
+    generator = function(props)
+        return Dictionary.join({
             items = {
                 -- {itemId = "matWood", dropRange = {min = 4, max = 10}, dropRate = 1.00}
             },
             cash = 1,
-        }
+        }, props)
     end,
 })
--- src/common/RecsComponents/NPCDriver.lua
+++ src/common/RecsComponents/NPCDriver.lua
@@ -3,17 +3,21 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage:WaitForChild("lib")
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
 
 local RECS = require(lib:WaitForChild("RECS"))
 
 return RECS.defineComponent({
     name = "NPCDriver",
-    generator = function()
-        return {
+    generator = function(props)
+        return Dictionary.join({
             gravityWeight = 1,
             targetVelocity = Vector3.new(0,0,0),
             targetDirection = Vector3.new(0,0,-1),
             maxMoveForce = Vector3.new(2000,0,2000),
-        }
+        },props)
     end,
 })
