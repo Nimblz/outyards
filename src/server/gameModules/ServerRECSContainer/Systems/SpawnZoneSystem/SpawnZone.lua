24af8169f3e87fab3aa36b18dc6e076ecd46a5d8
Nimblz <areuschle1@gmail.com>
2019-10-04 09:41:00 -0700

    Adjusts mob spawning position

--- src/server/gameModules/ServerRECSContainer/Systems/SpawnZoneSystem/SpawnZone.lua
+++ src/server/gameModules/ServerRECSContainer/Systems/SpawnZoneSystem/SpawnZone.lua
@@ -48,7 +48,7 @@ local function createThresholdArray(ratioTable)
 end
 
 local function getRandomPointInPart(part)
-	local point = Vector3.new(math.random()*2 - 1,0,math.random()*2 - 1)
+	local point = Vector3.new(math.random()*2 - 1,-1,math.random()*2 - 1)
 	local pointPos = (point/2) * part.Size
 	return (part.CFrame * CFrame.new(pointPos)).p
 end
