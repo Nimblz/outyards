1e9c7629f610a79947540690fd88dee9370d30f5
Nimblz <areuschle@gmail.com>
2019-10-07 19:02:06 -0700

    Implement equipment renderers and behaviors

---
+++ src/common/Items/equipment/tier1/weapon/StoneSword.lua
@@ -0,0 +1,29 @@
+return {
+    id = "swordStone",
+    name = "Stone Shortsword",
+    desc = "More of club than a sword",
+    tier = 1,
+
+    equipmentType = "weapon",
+    behaviorType = "sword",
+    rendererType = "oneHandedWeapon",
+
+    spriteSheet = "weapon",
+    spriteCoords = Vector2.new(1,1),
+
+    onlyOne = true,
+    recipe = {
+        wood = 10,
+        stone = 15,
+    },
+
+    stats = {
+        baseDamage = 6,
+    },
+
+    metadata = {
+        fireRate = 2,
+        attackDepth = 7,
+        attackArc = 45, -- in degrees
+    }
+}
