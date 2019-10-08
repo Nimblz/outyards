0c50b049ec90214f56cb473d74741400e6e57f67
Nimblz <areuschle1@gmail.com>
2019-10-07 19:02:06 -0700

    Implement equipment renderers and behaviors

---
+++ src/common/EquipmentRenderers/OneHandedWeapon.lua
@@ -0,0 +1,37 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local getItemModel = require(util:WaitForChild("getItemModel"))
+
+local renderer = {
+    id = "oneHandedWeapon"
+}
+
+function renderer:create()
+    local character = self.player.character or self.player.CharacterAdded:wait()
+    local weaponModel = getItemModel(self.itemId)
+    local rightHand = character:WaitForChild("RightHand")
+    local gripAttachment = weaponModel:FindFirstChild("grip")
+    assert(gripAttachment, "no grip for model: "..self.itemId)
+
+    local weld = Instance.new("ManualWeld")
+    weld.Part0 = weaponModel
+    weld.Part1 = rightHand
+
+    weld.C0 = gripAttachment.CFrame
+    weld.C1 = rightHand:FindFirstChild("RightGripAttachment").CFrame
+
+    weaponModel.Parent = character
+    weaponModel.Anchored = false
+    weld.Parent = weaponModel
+
+    self.model = weaponModel
+end
+
+function renderer:destroy()
+    self.model:destroy()
+end
+
+return renderer
