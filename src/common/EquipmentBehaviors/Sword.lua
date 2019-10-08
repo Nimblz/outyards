c32252d249ec937c4bd0d7511dfc18684fec4204
Nimblz <areuschle@gmail.com>
2019-10-08 12:00:29 -0700

    Implement sword animations

--- src/common/EquipmentBehaviors/Sword.lua
+++ src/common/EquipmentBehaviors/Sword.lua
@@ -1,13 +1,41 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local lib = ReplicatedStorage:WaitForChild("lib")
+local event = ReplicatedStorage:WaitForChild("event")
+
+local Animations = require(common:WaitForChild("Animations"))
+
+local eAttackActor = event:WaitForChild("eAttackActor")
+
 local behavior = {
     id = "sword"
 }
 
 function behavior:create()
+    local humanoid = self.character:FindFirstChild("Humanoid")
+    if not humanoid then return end
+    local slashAnimation = Animations.swing
+    local backSlashAnimation = Animations.backswing
+
+    self.swingTrack = humanoid:LoadAnimation(slashAnimation)
+    self.backSwingTrack = humanoid:LoadAnimation(backSlashAnimation)
 end
 
 function behavior:activated(props)
     self.props = props
-    -- props contains things like target direction, and any other metadata involved with activation
+    local mouse = props.mouse
+    if not self.doBackSwing then
+        self.doBackSwing = true
+        self.backSwingTrack:stop()
+        self.swingTrack:play()
+    else
+        self.doBackSwing = false
+        self.swingTrack:stop()
+        self.backSwingTrack:play()
+    end
+
+    self.character.UpperTorso.BrickColor = BrickColor.new("Bright green")
 end
 
 function behavior:deactivated()
