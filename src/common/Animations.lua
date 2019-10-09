360d856e8e8b9834b24b8b2493e1ab0e988c36d0
Nimblz <areuschle1@gmail.com>
2019-10-09 09:49:14 -0700

    add holding animation

--- src/common/Animations.lua
+++ src/common/Animations.lua
@@ -6,6 +6,7 @@ local function newAnimation(name,id)
 end
 
 return {
+    toolhold = newAnimation("hold","rbxassetid://507768375"),
     swing = newAnimation("swing", "rbxassetid://667286531"),
     backswing = newAnimation("backswing", "rbxassetid://667318897"),
     pickswing = newAnimation("pickswing", "rbxassetid://666865136"),
