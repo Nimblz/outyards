a0c54a0a84fe3d83c1d85c4c5a433cc9afa55778
Nimblz <areuschle@gmail.com>
2019-10-09 09:49:30 -0700

    Add damage logic

--- src/common/EquipmentBehaviors/Sword.lua
+++ src/common/EquipmentBehaviors/Sword.lua
@@ -1,4 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
 
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
@@ -17,9 +18,11 @@ function behavior:create()
     if not humanoid then return end
     local slashAnimation = Animations.swing
     local backSlashAnimation = Animations.backswing
+    local holdAnimation = Animations.toolhold
 
     self.swingTrack = humanoid:LoadAnimation(slashAnimation)
     self.backSwingTrack = humanoid:LoadAnimation(backSlashAnimation)
+    self.holdTrack = humanoid:LoadAnimation(holdAnimation)
 end
 
 function behavior:activated(props)
@@ -35,7 +38,22 @@ function behavior:activated(props)
         self.backSwingTrack:play()
     end
 
-    self.character.UpperTorso.BrickColor = BrickColor.new("Bright green")
+    local character = self.character
+    local rootPart = character.PrimaryPart
+    local rootCF = rootPart.CFrame
+    local targetCFrame = rootCF * CFrame.new(0,0,-5)
+
+    -- find npcs
+    local cornerOffset = Vector3.new(1,1,1)*8
+    local topCorner = targetCFrame.p + cornerOffset
+    local bottomCorner = targetCFrame.p - cornerOffset
+    local testRegion = Region3.new(bottomCorner,topCorner)
+
+    local parts = workspace:FindPartsInRegion3WithWhiteList(testRegion, CollectionService:GetTagged("ActorStats"))
+
+    for _,v in pairs(parts) do
+        eAttackActor:FireServer(v)
+    end
 end
 
 function behavior:deactivated()
@@ -49,9 +67,11 @@ function behavior:update()
 end
 
 function behavior:equipped()
+    self.holdTrack:play()
 end
 
 function behavior:unequipped()
+    self.holdTrack:stop()
 end
 
 function behavior:destroy()
