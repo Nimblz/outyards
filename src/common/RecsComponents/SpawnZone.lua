794fedd41f2727e0049a03304d58e91569e9da03
Nimblz <areuschle@gmail.com>
2019-10-10 10:46:52 -0700

    Lower default spawn cap

--- src/common/RecsComponents/SpawnZone.lua
+++ src/common/RecsComponents/SpawnZone.lua
@@ -15,7 +15,7 @@ return RECS.defineComponent({
             spawnables = {},
             spawnRate = 1, -- every n ticks
             counter = 0, -- when counter == rate, cointer gets set to 0 and mob is spawned
-            spawnCap = 100,
+            spawnCap = 30,
         },props)
     end,
 })
