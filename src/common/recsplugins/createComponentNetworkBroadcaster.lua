9e78350b18a690434618b3d3501ffcdc10c21056
Nimblz <areuschle@gmail.com>
2019-10-10 21:19:24 -0700

    Add recs networking plugins

--- src/common/recsplugins/createComponentNetworkBroadcaster.lua
+++ src/common/recsplugins/createComponentNetworkBroadcaster.lua
@@ -1,1 +1,70 @@
 -- broadcasts component changed events for replication
+
+local blacklist = {
+    changed = true,
+    raisePropertyChanged = true,
+    className = true,
+    entityFilter = true,
+    __index = true,
+}
+
+local function toNetworkable(component)
+    local className = component.className
+    local networkedComponent = {}
+    for key, value in pairs(component) do
+        if not blacklist[key] and typeof(value) ~= "function" then
+            networkedComponent[key] = value
+        end
+    end
+
+    return className, networkedComponent
+end
+
+local function createPlugin(addedEvent, changedEvent, removedEvent)
+    local broadcastPlugin = {}
+
+    local function fireAdded(player, instance, component)
+        local className, networkedComponent = toNetworkable(component)
+        addedEvent:FireClient(player, instance, className, networkedComponent)
+    end
+
+    function broadcastPlugin:componentAdded(core, instance, component)
+        if not component.replicates then return end
+        local className, networkedComponent = toNetworkable(component)
+
+        addedEvent:FireAllClients(instance, className, networkedComponent)
+        component.changed:connect(function(key, newValue, oldValue)
+            changedEvent:FireAllClients(instance, className, key, newValue)
+        end)
+    end
+
+    function broadcastPlugin:componentRemoving(core, instance,component)
+        if not component.replicates then return end
+        removedEvent:FireAllClients(instance,component.className)
+    end
+
+    function broadcastPlugin:coreInit(core)
+        local Players = game:GetService("Players")
+
+        local function playerAdded(newPlayer)
+            -- for each entity, replicate its components x-x (is this too much?)
+            for _, entityPairs in pairs(core._components) do
+                for entity, component in pairs(entityPairs) do
+                    if component.replicates then
+                        fireAdded(newPlayer, entity, component)
+                    end
+                end
+            end
+        end
+
+        Players.PlayerAdded:connect(playerAdded)
+
+        for _, player in pairs(Players:GetPlayers()) do
+            playerAdded(player)
+        end
+    end
+
+    return broadcastPlugin
+end
+
+return createPlugin
--- src/common/recsplugins/createComponentNetworkSubscriber.lua
+++ src/common/recsplugins/createComponentNetworkSubscriber.lua
@@ -1,1 +1,35 @@
 -- recieves component changed events for replication
+local function createPlugin(addedEvent,changedEvent,removedEvent)
+    local subscriberPlugin = {}
+
+    function subscriberPlugin:afterStepperStart(core)
+        addedEvent.OnClientEvent:connect(function(instance, className, props)
+            if not instance then return end
+            local componentIdentifier = core:getComponentClass(className)
+            if componentIdentifier then
+                core:addComponent(instance, componentIdentifier, props)
+            end
+        end)
+
+        changedEvent.OnClientEvent:connect(function(instance, className, key, newValue)
+            if not instance then return end
+            local componentIdentifier = core:getComponentClass(className)
+            local component = core:getComponent(instance,componentIdentifier)
+            if component then
+                component:updateProperty(key,newValue)
+            end
+        end)
+
+        removedEvent.OnClientEvent:connect(function(instance, className)
+            if not instance then return end
+            local componentIdentifier = core:getComponentClass(className)
+            if core:getComponent(instance,componentIdentifier) then
+                core:removeComponent(instance, componentIdentifier)
+            end
+        end)
+    end
+
+    return subscriberPlugin
+end
+
+return createPlugin
