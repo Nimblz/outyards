ea7e3d3e4fab61d928bc0efe4be80fb3f062b7c0
Nimblz <areuschle1@gmail.com>
2019-10-13 14:31:22 -0700

    Add fireTime to projectile component

--- src/common/RecsComponents/Projectile.lua
+++ src/common/RecsComponents/Projectile.lua
@@ -9,10 +9,11 @@ return RECS.defineComponent({
     generator = function(props)
         props = props or {}
         return {
-            id = "bullet",
+            id = props.id or "bullet",
             position = props.position or Vector3.new(0,0,0), -- per sec
             velocity = props.velocity or Vector3.new(0,0,0),
             gravityScale = props.gravityScale or 1,
+            fireTime = props.fireTime or 0,
             owner = nil, -- player who owns this projectile, nil if server owned
             damagesPlayers = false, -- does this projectile damage players? server created projectiles often will
         }
