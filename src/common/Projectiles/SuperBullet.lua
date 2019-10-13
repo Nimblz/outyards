37e263f30d5de2fc4190635215e2887e184bdff5
Nimblz <areuschle1@gmail.com>
2019-10-13 14:32:52 -0700

    Add testing gun

---
+++ src/common/Projectiles/SuperBullet.lua
@@ -0,0 +1,62 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CollectionService = game:GetService("CollectionService")
+
+local event = ReplicatedStorage:WaitForChild("event")
+local common = ReplicatedStorage:WaitForChild("common")
+
+local ParticleCreator = require(common:WaitForChild("ParticleCreator"))
+
+local eAttackActor = event:WaitForChild("eAttackActor")
+
+return {
+    id = "superbullet",
+    speed = 1024,
+    gravityScale = 1,
+
+    onFire = function(entity, component, pzCore)
+        ParticleCreator.spawnParticle("spark", {
+            cFrame = entity.CFrame,
+            scale = 0.3,
+            amount = 1
+        })
+    end,
+
+    onUpdate = function(entity, component, pzCore)
+        entity.Color = Color3.fromHSV((tick() - component.fireTime or 0)%1,0.75,1)
+    end,
+
+    onHit = function(entity, component, pzCore, hit, hitPos, normal)
+        -- if its an enemy do damage
+
+        local scaleMod = math.random()
+
+        ParticleCreator.spawnParticle("circle", {
+            cFrame = entity.CFrame,
+            scale = 1+scaleMod,
+            amount = 1
+        })
+
+        ParticleCreator.spawnParticle("ring", {
+            cFrame = entity.CFrame,
+            scale = 3+scaleMod,
+            amount = 1
+        })
+            -- find npcs
+        local cornerOffset = Vector3.new(1,1,1)*16
+        local topCorner = entity.CFrame.p + cornerOffset
+        local bottomCorner = entity.CFrame.p - cornerOffset
+        local testRegion = Region3.new(bottomCorner,topCorner)
+
+        local parts = workspace:FindPartsInRegion3WithWhiteList(testRegion, CollectionService:GetTagged("ActorStats"))
+
+        if CollectionService:HasTag(hit, "ActorStats") then
+            eAttackActor:FireServer(hit)
+        end
+
+        for _,v in pairs(parts) do
+            if v ~= hit then
+                eAttackActor:FireServer(v)
+            end
+        end
+    end,
+}
