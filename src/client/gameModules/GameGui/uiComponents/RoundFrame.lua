933d26cc2ae3478b4f5766b1e866628036ea54a0
Nim <areuschle1@gmail.com>
2019-10-12 23:59:52 -0700

    Add rounded frame

---
+++ src/client/gameModules/GameGui/uiComponents/RoundFrame.lua
@@ -0,0 +1,32 @@
+-- frame with rounded corners
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local lib = ReplicatedStorage:WaitForChild("lib")
+local util = common:WaitForChild("util")
+
+local Roact = require(lib:WaitForChild("Roact"))
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
+
+return function(props)
+    local elementProps = Dictionary.join(props, {
+        class = Dictionary.None,
+        color = Dictionary.None,
+    })
+
+    elementProps.ImageColor3 = (
+        props.color or
+        props.ImageColor3 or
+        props.BackgroundColor3 or
+        Color3.new(1,1,1)
+    )
+
+    return Roact.createElement(props.class or "ImageLabel", Dictionary.join({
+        BackgroundTransparency = 1,
+        BorderSizePixel = 0,
+        Image = "rbxassetid://4103149690",
+        ScaleType = Enum.ScaleType.Slice,
+        SliceCenter = Rect.new(16,16,16,16),
+    }, elementProps))
+end
