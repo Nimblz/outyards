04cd31ca1b290dd3dd0cbcd235b0ad5009d693e1
Nimblz <areuschle@gmail.com>
2019-10-13 14:32:13 -0700

    Move CashLabel to menubar

--- src/client/gameModules/GameGui/uiComponents/MenuBar/CashLabel.lua
+++ src/client/gameModules/GameGui/uiComponents/MenuBar/CashLabel.lua
@@ -4,17 +4,18 @@ local LocalPlayer = Players.LocalPlayer
 
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
-local event = ReplicatedStorage:WaitForChild("event")
+local component = script:FindFirstAncestor("uiComponents")
 
 local Roact = require(lib:WaitForChild("Roact"))
 local RoactRodux = require(lib:WaitForChild("RoactRodux"))
 local Selectors = require(common:WaitForChild("Selectors"))
 
+local RoundFrame = require(component:WaitForChild("RoundFrame"))
 local CashLabel = Roact.Component:extend("CashLabel")
 
-local HORIZONTAL_SIZE = 24*3*4
-local VERTICAL_SIZE = HORIZONTAL_SIZE/4
-local PADDING = 24
+local HORIZONTAL_SIZE = 200
+local VERTICAL_SIZE = 72
+local PADDING = 16
 
 function CashLabel:init()
 end
@@ -31,36 +32,35 @@ function CashLabel:render()
             SortOrder = Enum.SortOrder.LayoutOrder,
             VerticalAlignment = Enum.VerticalAlignment.Center,
             HorizontalAlignment = Enum.HorizontalAlignment.Left,
-            Padding = UDim.new(0,PADDING/2),
+            Padding = UDim.new(0,PADDING),
             FillDirection = Enum.FillDirection.Horizontal,
         }),
-        coinThumbnail = Roact.createElement("ImageLabel", {
+        padding = Roact.createElement("UIPadding", {
+            PaddingLeft = UDim.new(0,12)
+        }),
+        icon = Roact.createElement("ImageLabel", {
             Image = "rbxassetid://3977083793",
-            Size = UDim2.new(1,0,1,0),
-            SizeConstraint = Enum.SizeConstraint.RelativeYY,
+            Size = UDim2.new(0,48,0,48),
             BackgroundTransparency = 1,
             LayoutOrder = 1,
         }),
         coinText = Roact.createElement("TextLabel", {
             Text = (cashValue and "$"..tostring(math.floor(cashValue))) or "N/A",
-            Size = UDim2.new(1,-((VERTICAL_SIZE)+PADDING),1,0),
+            Size = UDim2.new(0,0,1,0),
             BackgroundTransparency = 1,
             TextColor3 = Color3.new(1,1,1),
-            TextStrokeTransparency = 0,
+            TextStrokeTransparency = 0.5,
             LayoutOrder = 2,
-            TextSize = PADDING,
+            TextSize = 32,
             TextXAlignment = Enum.TextXAlignment.Left,
             Font = Enum.Font.GothamBlack,
         })
     }
 
-    return Roact.createElement("Frame", {
+    return Roact.createElement(RoundFrame, {
         Size = UDim2.new(0,HORIZONTAL_SIZE,0,VERTICAL_SIZE),
         Position = UDim2.new(0,PADDING,1,-PADDING),
         AnchorPoint = Vector2.new(0,1),
-
-        BackgroundTransparency = 1,
-        BorderSizePixel = 0,
     }, children)
 end
 
