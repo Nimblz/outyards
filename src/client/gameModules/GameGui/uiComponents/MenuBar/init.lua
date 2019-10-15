a5ec73fa96e1b1ad6eacae181ca6c33d5b4a4436
Nimblz <areuschle1@gmail.com>
2019-10-14 22:35:40 -0700

    Implement new view system

--- src/client/gameModules/GameGui/uiComponents/MenuBar/init.lua
+++ src/client/gameModules/GameGui/uiComponents/MenuBar/init.lua
@@ -5,43 +5,49 @@ local lib = ReplicatedStorage:WaitForChild("lib")
 local event = ReplicatedStorage:WaitForChild("event")
 
 local Roact = require(lib:WaitForChild("Roact"))
+local RoactRodux = require(lib:WaitForChild("RoactRodux"))
 
-local MenuBar = Roact.Component:extend("MenuBar")
+local Selectors = require(common:WaitForChild("Selectors"))
+local Thunks = require(common:WaitForChild("Thunks"))
+
+local MenuBar = Roact.PureComponent:extend("MenuBar")
 local MenuButton = require(script:WaitForChild("MenuButton"))
 local CashLabel = require(script:WaitForChild("CashLabel"))
 
 local PADDING = 16
 
-function MenuBar:init()
-end
-
-function MenuBar:didMount()
-end
-
 function MenuBar:render()
-    local menuButtons = self.props.menuButtons or {
-        boosts = {
-            icon = "rbxassetid://4102976956",
+    local visible = self.props.visible
+    local currentView = self.props.currentView
+    local buttons = {}
+
+    local menuButtons = {
+        inventory = {
+            icon = "rbxassetid://666448883",
+            name = "Inventory",
             layoutOrder = 2
         },
         crafting = {
             icon = "rbxassetid://666448950",
+            name = "Crafting",
             layoutOrder = 3
         },
-        inventory = {
-            icon = "rbxassetid://666448883",
+        boosts = {
+            icon = "rbxassetid://4102976956",
+            name = "Premium Shop",
             layoutOrder = 4
         },
-        codes = {
-            icon = "rbxassetid://391745819",
-            layoutOrder = 5
-        },
+        -- codes = {
+        --     icon = "rbxassetid://391745819",
+        --     name = "Social Codes",
+        --     layoutOrder = 5
+        -- },
         options = {
             icon = "rbxassetid://282366832",
-            layoutOrder = 6
+            name = "Options",
+            layoutOrder = 5
         },
     }
-    local buttons = {}
 
     buttons.layout = Roact.createElement("UIListLayout", {
         SortOrder = Enum.SortOrder.LayoutOrder,
@@ -54,6 +60,10 @@ function MenuBar:render()
     buttons.cash = Roact.createElement(CashLabel)
 
     for idx,buttonProps in pairs(menuButtons) do
+        buttonProps.callback = function()
+            self.props.setView(currentView, idx)
+        end
+        buttonProps.active = currentView == idx
         local newButton = Roact.createElement(MenuButton, buttonProps)
         buttons["button_"..idx] = newButton
     end
@@ -63,7 +73,29 @@ function MenuBar:render()
         Position = UDim2.new(0,PADDING,1,-PADDING),
         AnchorPoint = Vector2.new(0,1),
         BackgroundTransparency = 1,
+        Visible = visible
     }, buttons)
 end
+
+local function mapStateToProps(state,props)
+    return {
+        currentView = Selectors.getView(state),
+        visible = Selectors.getNavbarVisible(state),
+    }
+end
+
+local function mapDispatchToProps(dispatch)
+    return {
+        setView = function(currentView,newView)
+            if currentView == newView then
+                dispatch(Thunks.VIEW_SET("default"))
+            else
+                dispatch(Thunks.VIEW_SET(newView))
+            end
+        end
+    }
+end
+
+MenuBar = RoactRodux.connect(mapStateToProps, mapDispatchToProps)(MenuBar)
 
 return MenuBar
--- src/client/gameModules/GameGui/uiComponents/RoundButton.lua
+++ src/client/gameModules/GameGui/uiComponents/RoundButton.lua
@@ -40,22 +40,50 @@ function RoundButton:render()
     local hovered = self.state.hovered or false
     local pressed = self.state.pressed or false
 
-    local normalColor = Color3.new(1,1,1)
-    local hoveredColor = Color3.new(0.9,0.9,0.9)
-    local pressedColor = Color3.new(0.8,0.8,0.8)
+    local normalColor = props.color or props.ImageColor3 or props.BackgroundColor3 or Color3.new(1,1,1)
+    local hoveredColor = props.hoveredColor or normalColor:lerp(Color3.fromRGB(0, 0, 0),0.1)
+    local pressedColor = props.pressedColor or normalColor:lerp(Color3.fromRGB(0, 0, 0),0.25)
+    local finalColor = (pressed and pressedColor) or (hovered and hoveredColor) or normalColor
+
+    local inputFuncs = {
+        mouseEnter = props[Roact.Event.MouseEnter],
+        mouseLeave = props[Roact.Event.MouseLeave],
+        mouseDown = props[Roact.Event.MouseButton1Down],
+        mouseUp = props[Roact.Event.MouseButton1Up],
+    }
+
+    local function callInputFunc(name)
+        if inputFuncs[name] then
+            inputFuncs[name]()
+        end
+    end
 
     local elementProps = Dictionary.join(props, {
         color = Dictionary.None,
+        hoveredColor = Dictionary.None,
+        pressedColor = Dictionary.None,
+
+        ImageColor3 = finalColor,
+
+        [Roact.Event.MouseEnter] = function()
+            self:setHovered(true)
+            callInputFunc("mouseEnter")
+        end,
+        [Roact.Event.MouseLeave] = function()
+            self:setHovered(false)
+            self:setPressed(false)
+            callInputFunc("mouseLeave")
+        end,
+        [Roact.Event.MouseButton1Down] = function()
+            self:setPressed(true)
+            callInputFunc("mouseDown")
+        end,
+        [Roact.Event.MouseButton1Up] = function()
+            self:setPressed(false)
+            self:setHovered(false)
+            callInputFunc("mouseUp")
+        end,
     })
-
-    props.color = (pressed and pressedColor) or (hovered and hoveredColor) or normalColor
-
-    elementProps.ImageColor3 = (
-        props.color or
-        props.ImageColor3 or
-        props.BackgroundColor3 or
-        Color3.new(1,1,1)
-    )
 
     return Roact.createElement("ImageButton", Dictionary.join({
         BackgroundTransparency = 1,
@@ -63,10 +91,6 @@ function RoundButton:render()
         Image = "rbxassetid://4103149690",
         ScaleType = Enum.ScaleType.Slice,
         SliceCenter = Rect.new(16,16,16,16),
-        [Roact.Event.MouseEnter] = function() self:setHovered(true) end,
-        [Roact.Event.MouseLeave] = function() self:setHovered(false) self:setPressed(false) end,
-        [Roact.Event.MouseButton1Down] = function() self:setPressed(true) end,
-        [Roact.Event.MouseButton1Up] = function() self:setPressed(false) end,
     }, elementProps))
 end
 
