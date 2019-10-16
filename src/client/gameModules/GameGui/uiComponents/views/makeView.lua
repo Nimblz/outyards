996d1d556d9f243febd896439026a4ec1a44938a
Nimblz <areuschle1@gmail.com>
2019-10-15 18:39:47 -0700

    Handle props

--- src/client/gameModules/GameGui/uiComponents/views/makeView.lua
+++ src/client/gameModules/GameGui/uiComponents/views/makeView.lua
@@ -2,22 +2,33 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
+local util = common:WaitForChild("util")
 
+local Dictionary = require(util:WaitForChild("Dictionary"))
 local Selectors = require(common:WaitForChild("Selectors"))
 local Roact = require(lib:WaitForChild("Roact"))
 local RoactRodux = require(lib:WaitForChild("RoactRodux"))
 
 local function makeView(component, viewId)
     -- wraps component in a new component that is only visible when its viewid is visible
-    local newView = Roact.PureComponent:extend("view_"..viewId)
+    local newView = Roact.Component:extend("view_"..viewId)
 
     function newView:render()
+
+        local prunedProps = Dictionary.join(self.props, {
+            visible = Dictionary.None,
+        })
+
         return Roact.createElement("Frame", {
             Selectable = false,
             Active = false,
             BackgroundTransparency = 1,
 
-            Visible = self.props.visible,
+            Size = UDim2.new(1,0,1,0),
+
+            Visible = self.props.visible or false,
+        }, {
+            view = Roact.createElement(component, prunedProps)
         })
     end
 
