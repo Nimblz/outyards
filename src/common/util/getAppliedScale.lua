a9ea538cb6ceadb8417494ad7e9b7e64e208f532
Nimblz <areuschle@gmail.com>
2019-10-16 16:35:04 -0700

    Add getAppliedScale

---
+++ src/common/util/getAppliedScale.lua
@@ -0,0 +1,32 @@
+-- searches for all scale objects that could be affecting passed instance
+local function getChildrenOfClass(instance, className)
+	local result = {}
+	for _, child in pairs(instance:GetChildren()) do
+		if child:IsA(className) then
+			table.insert(result, child)
+		end
+	end
+	return result
+end
+
+local function getAppliedScale(instance)
+    local finalScale = 1
+    local currentPosition = instance
+
+	while true do
+        currentPosition = currentPosition.Parent
+
+		local scales = getChildrenOfClass(instance, "UIScale")
+		for _, scaleInstance in pairs(scales) do
+			finalScale = finalScale * scaleInstance.Scale
+        end
+
+        if not currentPosition.Parent then
+            break
+        end
+	end
+
+	return finalScale
+end
+
+return getAppliedScale
