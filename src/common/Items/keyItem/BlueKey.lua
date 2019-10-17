b20c90b18657337fdfd335453794ced333b6f20b
Nimblz <areuschle@gmail.com>
2019-10-16 17:00:35 -0700

    Remove key recipes

--- src/common/Items/keyItem/BlueKey.lua
+++ src/common/Items/keyItem/BlueKey.lua
@@ -1,14 +1,7 @@
 return {
     id = "blueKey",
     name = "Blue Key",
-    tier = 302,
+    tier = 1,
     spriteSheet = "keyItems",
     spriteCoords = Vector2.new(2,1),
-
-    onlyOne = true,
-
-    recipe = {
-        ingotIron = 30,
-        ingotCopper = 30,
-    }
 }
