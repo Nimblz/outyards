2810327f77b7d15b9addf8303638e51453d1757a
Nimblz <areuschle1@gmail.com>
2019-10-21 13:52:01 -0700

    Add off-hand holding animation

--- src/common/Animations.lua
+++ src/common/Animations.lua
@@ -7,6 +7,7 @@ end
 
 return {
     toolhold = newAnimation("hold","rbxassetid://507768375"),
+    offHandHold = newAnimation("offHandHold","rbxassetid://04178278456"),
     swing = newAnimation("swing", "rbxassetid://04093489126"),
     backswing = newAnimation("backswing", "rbxassetid://04093501278"),
     pickswing = newAnimation("pickswing", "rbxassetid://04093472220"),
