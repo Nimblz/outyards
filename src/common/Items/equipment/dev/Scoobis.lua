d23e788c35911736abb8652b2ef11fd37474614d
Nimblz <areuschle1@gmail.com>
2019-10-21 13:56:40 -0700

    Add scoobis hat

---
+++ src/common/Items/equipment/dev/Scoobis.lua
@@ -0,0 +1,22 @@
+-- +defense
+
+return {
+    id = "scoobis",
+    name = "Scoobis",
+    desc = "Scoob!",
+    tier = 1,
+
+    equipmentType = "hat",
+    behaviorType = "none",
+    rendererType = "hat",
+
+    spriteSheet = "armor",
+    spriteCoords = Vector2.new(1,3),
+
+    stats = {
+        moveSpeed = 1000,
+        baseDamage = 99999,
+        attackRate = 10,
+        defense = 9999999999,
+    }
+}
