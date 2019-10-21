93d16c8a15f230ec4bda933be40989b2badcb2bf
Nimblz <areuschle1@gmail.com>
2019-10-21 13:55:18 -0700

    Add dominus aureus

---
+++ src/common/Items/equipment/dev/DominusAureus.lua
@@ -0,0 +1,19 @@
+-- +defense
+
+return {
+    id = "dominusaureus",
+    name = "Dominus Aureus",
+    tier = 1,
+
+    equipmentType = "hat",
+    behaviorType = "none",
+    rendererType = "hat",
+
+    spriteSheet = "armor",
+    spriteCoords = Vector2.new(5,5),
+
+    stats = {
+        moveSpeed = 10,
+        defense = 5000,
+    }
+}
