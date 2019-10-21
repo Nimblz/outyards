9e6c30d2d4ff1bc6aa4cff509c9d3816061a9db6
Nimblz <areuschle1@gmail.com>
2019-10-21 13:54:38 -0700

    Add armor renderer

---
+++ src/common/EquipmentRenderers/Armor.lua
@@ -0,0 +1,42 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local common = ReplicatedStorage:WaitForChild("common")
+local util = common:WaitForChild("util")
+
+local getItemModel = require(util:WaitForChild("getItemModel"))
+
+local renderer = {
+    id = "armor"
+}
+
+function renderer:create()
+    local character = self.player.character or self.player.CharacterAdded:wait()
+    local model = getItemModel(self.itemId)
+    local upperTorso = character:WaitForChild("UpperTorso")
+    local gripAttachment = model:FindFirstChild("attachment")
+    assert(gripAttachment, "no attachment for model: "..self.itemId)
+
+    local weld = Instance.new("ManualWeld")
+    weld.Part0 = model
+    weld.Part1 = upperTorso
+
+    weld.C0 = gripAttachment.CFrame
+    weld.C1 = upperTorso:WaitForChild("NeckRigAttachment").CFrame
+
+    model.Parent = character
+    model.Anchored = false
+    model.CanCollide = false
+    weld.Parent = model
+
+    local modelMesh = model:WaitForChild("Mesh")
+    local worldSize = model:WaitForChild("MeshWorldSize").Value
+    modelMesh.Scale = ((upperTorso.Size + Vector3.new(0,0.25,0)) / worldSize) + Vector3.new(0.1,0.1,0.15)
+
+    self.model = model
+end
+
+function renderer:destroy()
+    self.model:destroy()
+end
+
+return renderer
