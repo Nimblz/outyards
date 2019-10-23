fb939201c10112664a25cd3283e47329430cefa4
Nimblz <areuschle1@gmail.com>
2019-10-23 14:30:24 -0700

    Add iron sword

---
+++ src/common/Items/equipment/tier2/weapon/IronSword.lua
@@ -0,0 +1,27 @@
+return {
+    id = "swordIron",
+    name = "Iron Sword",
+    tier = 1,
+
+    equipmentType = "weapon",
+    behaviorType = "melee",
+    rendererType = "oneHandedWeapon",
+
+    spriteSheet = "weapon",
+    spriteCoords = Vector2.new(1,1),
+
+    onlyOne = true,
+    recipe = {
+        ingotIron = 10,
+    },
+
+    stats = {
+        baseDamage = 8,
+    },
+
+    metadata = {
+        fireRate = 3,
+        attackRange = 9,
+        attackArc = 120, -- in degrees
+    }
+}
