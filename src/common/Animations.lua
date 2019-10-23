67660d9ec989bc992906c3889e67f275c88a0559
Nimblz <areuschle1@gmail.com>
2019-10-23 14:27:26 -0700

    Add r6 animations

--- src/common/Animations.lua
+++ src/common/Animations.lua
@@ -11,4 +11,8 @@ return {
     swing = newAnimation("swing", "rbxassetid://04093489126"),
     backswing = newAnimation("backswing", "rbxassetid://04093501278"),
     pickswing = newAnimation("pickswing", "rbxassetid://04093472220"),
+
+    r6run = newAnimation("r6run", "rbxassetid://180426354"),
+    r6idle = newAnimation("r6idle", "rbxassetid://180435571"),
+    r6attack = newAnimation("r6attack", "rbxassetid://4192868719")
 }
