32f02d95ba9135366dc0fa26b33f7901af1b200e
Nimblz <areuschle1@gmail.com>
2019-10-23 14:30:16 -0700

    Add black knight sword

---
+++ src/common/Items/equipment/tier2/weapon/BlackKnightSword.lua
@@ -0,0 +1,22 @@
+return {
+    id = "blackKnightSword",
+    name = "Black Knight's Sword",
+    tier = 1,
+
+    equipmentType = "weapon",
+    behaviorType = "melee",
+    rendererType = "oneHandedWeapon",
+
+    spriteSheet = "weapon",
+    spriteCoords = Vector2.new(1,10),
+
+    stats = {
+        baseDamage = 12,
+    },
+
+    metadata = {
+        fireRate = 3,
+        attackRange = 12,
+        attackArc = 120, -- in degrees
+    }
+}
