0f231a11ed6f858da9407073d082340a3cd04f66
Nimblz <areuschle1@gmail.com>
2019-10-23 14:31:44 -0700

    Add group properties

--- src/common/RecsComponents/SpawnZone.lua
+++ src/common/RecsComponents/SpawnZone.lua
@@ -13,9 +13,11 @@ return RECS.defineComponent({
     generator = function(props)
         return Dictionary.join({
             spawnables = {},
-            spawnRate = 1, -- every n ticks
+            spawnRate = 20, -- every n ticks
             counter = 0, -- when counter == rate, counter gets set to 0 and mob is spawned
+            groupRadius = 32,
+            groupCount = NumberRange.new(5,10),
             spawnCap = 50,
-        },props)
+        }, props)
     end,
 })
