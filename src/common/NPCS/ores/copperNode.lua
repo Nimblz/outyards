306304d9b67a2dff7a1c856a239f991bbeee66b2
Nimblz <areuschle1@gmail.com>
2019-10-27 12:27:52 -0700

    Add ore nodes

---
+++ src/common/NPCS/ores/copperNode.lua
@@ -0,0 +1,27 @@
+return {
+    npcType = script.Name,
+    name = "Copper Ore",
+    propsGenerator = function()
+        return {
+            ActorStats = {
+                maxHealth = 10,
+                health = 10,
+                moveSpeed = 0,
+                baseDamage = 0
+            },
+            ItemDrops = {
+                items = {
+                    {itemId = "oreCopper", dropRange = {min = 5, max = 15}, dropRate = 1},
+                },
+                cash = 0,
+            },
+            AI = {
+                aiType = "NoAI",
+            }
+        }
+    end,
+    boundingBoxProps = {
+        Size = Vector3.new(6,4,6),
+        Color = Color3.fromRGB(84, 87, 88),
+    }
+}
---
+++ src/common/NPCS/ores/goldNode.lua
@@ -0,0 +1,27 @@
+return {
+    npcType = script.Name,
+    name = "Gold Ore",
+    propsGenerator = function()
+        return {
+            ActorStats = {
+                maxHealth = 40,
+                health = 40,
+                moveSpeed = 0,
+                baseDamage = 0
+            },
+            ItemDrops = {
+                items = {
+                    {itemId = "oreGold", dropRange = {min = 5, max = 15}, dropRate = 1},
+                },
+                cash = 0,
+            },
+            AI = {
+                aiType = "NoAI",
+            }
+        }
+    end,
+    boundingBoxProps = {
+        Size = Vector3.new(6,4,6),
+        Color = Color3.fromRGB(84, 87, 88),
+    }
+}
