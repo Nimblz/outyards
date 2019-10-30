f8d14ee2a3a2c22358608083f4c6e2495e711e8f
Nimblz <areuschle1@gmail.com>
2019-10-29 18:50:19 -0700

    Fit on mount

--- src/client/gameModules/GameGui/uiComponents/FitList.lua
+++ src/client/gameModules/GameGui/uiComponents/FitList.lua
@@ -15,11 +15,63 @@ local FitList = Roact.Component:extend("FitList")
 
 function FitList:init()
 	self.sizeBinding, self.setSize = Roact.createBinding(UDim2.new())
+	self.listRef = Roact.createRef()
+end
+
+function FitList:refit(instance)
+	local fitAxes = self.props.fitAxes or "XY"
+	local containerProps = self.props.containerProps
+	local paddingProps = self.props.paddingProps
+	local minSize = self.props.minSize
+
+	if paddingProps ~= nil then
+		paddingProps = Dictionary.merge({
+			PaddingTop = UDim.new(0,0),
+			PaddingBottom = UDim.new(0,0),
+			PaddingLeft = UDim.new(0,0),
+			PaddingRight = UDim.new(0,0),
+		}, paddingProps)
+	end
+
+	local scale = getAppliedScale(instance)
+	local contentSize = instance.AbsoluteContentSize / scale
+
+	if paddingProps ~= nil then
+		contentSize = contentSize + Vector2.new(
+			paddingProps.PaddingLeft.Offset + paddingProps.PaddingRight.Offset,
+			paddingProps.PaddingTop.Offset + paddingProps.PaddingBottom.Offset
+		)
+	end
+
+	if minSize then
+		contentSize = Vector2.new(
+			math.max(contentSize.X, minSize.X),
+			math.max(contentSize.Y, minSize.Y)
+		)
+	end
+
+	local combinedSize
+
+	if fitAxes == "X" then
+		combinedSize = UDim2.new(0, contentSize.X, containerProps.Size.Y.Scale, containerProps.Size.Y.Offset)
+	elseif fitAxes == "Y" then
+		combinedSize = UDim2.new(containerProps.Size.X.Scale, containerProps.Size.X.Offset, 0, contentSize.Y)
+	elseif fitAxes == "XY" then
+		combinedSize = UDim2.new(0, contentSize.X, 0, contentSize.Y)
+	else
+		error("Invalid fitAxes value")
+	end
+
+	self.setSize(combinedSize)
+end
+
+function FitList:didMount()
+	local instance = self.listRef:getValue()
+	self:refit(instance)
 end
 
 function FitList:render()
 	local containerKind = self.props.containerKind or "Frame"
-	local fitAxes = self.props.fitAxes or "XY"
 	local containerProps = self.props.containerProps
 	local layoutProps = self.props.layoutProps
 	local paddingProps = self.props.paddingProps
@@ -39,29 +91,9 @@ function FitList:render()
 		["$Layout"] = e("UIListLayout", Dictionary.merge({
 			SortOrder = Enum.SortOrder.LayoutOrder,
 			[Roact.Change.AbsoluteContentSize] = function(instance)
-				local scale = getAppliedScale(instance)
-				local contentSize = instance.AbsoluteContentSize / scale
-
-				if paddingProps ~= nil then
-					contentSize = contentSize + Vector2.new(
-						paddingProps.PaddingLeft.Offset + paddingProps.PaddingRight.Offset,
-						paddingProps.PaddingTop.Offset + paddingProps.PaddingBottom.Offset)
-				end
-
-				local combinedSize
-
-				if fitAxes == "X" then
-					combinedSize = UDim2.new(0, contentSize.X, containerProps.Size.Y.Scale, containerProps.Size.Y.Offset)
-				elseif fitAxes == "Y" then
-					combinedSize = UDim2.new(containerProps.Size.X.Scale, containerProps.Size.X.Offset, 0, contentSize.Y)
-				elseif fitAxes == "XY" then
-					combinedSize = UDim2.new(0, contentSize.X, 0, contentSize.Y)
-				else
-					error("Invalid fitAxes value")
-				end
-
-				self.setSize(combinedSize)
+				self:refit(instance)
 			end,
+			[Roact.Ref] = self.listRef
 		}, layoutProps)),
 
 		["$Padding"] = padding,
