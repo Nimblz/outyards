0d0315b04c6404df44b8e395fc2871f88822f62e
Nimblz <areuschle1@gmail.com>
2019-10-29 18:48:45 -0700

    Add voidmetal ingot

---
+++ src/common/Items/mats/tier7/IngotVoidmetal.lua
@@ -0,0 +1,17 @@
+return {
+    id = "ingotVoidmetal",
+    name = "Voidmetal Ingot",
+    desc = "Retains and enhances the properties of its components.",
+    tier = 8,
+
+    spriteSheet = "materials",
+    spriteCoords = Vector2.new(9,2),
+
+    recipe = {
+        ingotGold = 15,
+        ingotBluesteel = 15,
+        ingotViridium = 15,
+        ingotAdurite = 15,
+    },
+    craftQuantity = 5,
+}
