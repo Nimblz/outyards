147b133b9b3e4f5475ba96a68b5cb8be5c2223fe
Nimblz <areuschle1@gmail.com>
2019-10-31 14:40:17 -0700

    Add snowball event item

---
+++ src/common/Items/mats/misc/Snowball.lua
@@ -0,0 +1,12 @@
+return {
+    id = "snowball",
+    name = "Snowball",
+    tier = 1,
+    spriteSheet = "materials",
+    spriteCoords = Vector2.new(2,4),
+
+    tags = {
+        "material",
+        "event",
+    }
+}
