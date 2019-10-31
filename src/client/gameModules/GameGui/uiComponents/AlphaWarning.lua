9c4c9ef7bc206a3da4674fb5d437592f2b21a186
Nimblz <areuschle1@gmail.com>
2019-10-30 19:04:55 -0700

    Add fade-in

--- src/client/gameModules/GameGui/uiComponents/AlphaWarning.lua
+++ src/client/gameModules/GameGui/uiComponents/AlphaWarning.lua
@@ -5,6 +5,7 @@ local lib = ReplicatedStorage:WaitForChild("lib")
 local event = ReplicatedStorage:WaitForChild("event")
 local component = script:FindFirstAncestor("uiComponents")
 
+local Otter = require(lib:WaitForChild("Otter"))
 local Roact = require(lib:WaitForChild("Roact"))
 
 local FullScreenModal = require(component:WaitForChild("FullScreenModal"))
@@ -13,29 +14,53 @@ local FitText = require(component:WaitForChild("FitText"))
 local RoundFrame = require(component:WaitForChild("RoundFrame"))
 local FancyButton = require(component:WaitForChild("FancyButton"))
 
-local AlphaWarning = Roact.Component:extend("AlphaWarning")
+local AlphaWarning = Roact.PureComponent:extend("AlphaWarning")
+
+local FADE_TIME = 0.2
 
 function AlphaWarning:init()
+    self.transparencyMotor = Otter.createSingleMotor(1)
+
     self:setState(function()
         return {
-            visible = true
+            transparency = 1,
+            visible = true,
         }
     end)
 end
 
 function AlphaWarning:hide()
-    self:setState(function()
-        return {
-            visible = false
-        }
+    self.transparencyMotor:setGoal(Otter.spring(1, {
+        frequency = 1/FADE_TIME
+    }))
+
+    self.transparencyMotor:onComplete(function ()
+        self:setState(function()
+            return {
+                visible = false,
+                transparency = 1,
+            }
+        end)
     end)
 end
 
 function AlphaWarning:didMount()
+    self.transparencyMotor:setGoal(Otter.spring(0, {
+        frequency = 1/FADE_TIME
+    }))
+
+    self.transparencyMotor:onStep(function(value)
+        self:setState(function()
+            return {
+                transparency = value,
+            }
+        end)
+    end)
 end
 
 function AlphaWarning:render()
-    return self.state.visible and Roact.createElement(FullScreenModal, {}, {
+    local transparency = self.state.transparency
+    local warning = Roact.createElement(FullScreenModal, {transparency = transparency}, {
         centerFrame = Roact.createElement(FitList, {
             containerKind = RoundFrame,
             paddingProps = {
@@ -51,14 +76,16 @@ function AlphaWarning:render()
             containerProps = {
                 AnchorPoint = Vector2.new(0.5,0.5),
                 Position = UDim2.new(0.5,0,0.5,0),
+                ImageTransparency = transparency
             },
             minSize = Vector2.new(300,200),
         }, {
             heading = Roact.createElement(FitText, {
-                Text = "WARNING",
+                Text = "! ! ! WARNING ! ! !",
                 Font = Enum.Font.GothamBlack,
                 TextSize = 48,
                 LayoutOrder = 1,
+                TextTransparency = transparency,
             }),
             body = Roact.createElement(FitText, {
                 Text = "Outyards is in early alpha. Things will change a lot. Data may be wiped. Dont say I didn't warn you!",
@@ -67,6 +94,7 @@ function AlphaWarning:render()
                 TextWrapped = true,
                 fitAxis = "Y",
                 Size = UDim2.new(0,600,0,0),
+                TextTransparency = transparency,
             }),
             button = Roact.createElement(FitList, {
                 containerKind = FancyButton,
@@ -74,6 +102,7 @@ function AlphaWarning:render()
                     color = Color3.fromRGB(255, 162, 40),
                     Size = UDim2.new(0,200,0,50),
                     LayoutOrder = 3,
+                    ImageTransparency = transparency,
                     [Roact.Event.Activated] = function() self:hide() end
                 },
                 layoutProps = {
@@ -91,10 +120,14 @@ function AlphaWarning:render()
                 Roact.createElement(FitText, {
                     Text = "I understand",
                     TextSize = 24,
+                    TextTransparency = transparency,
                 })
             }),
         })
     })
+    if self.state.visible then
+        return warning
+    end
 end
 
 return AlphaWarning
