902c4120bdb0c6fba9704e418e96a1789c5b49c6
Nimblz <areuschle1@gmail.com>
2019-10-31 14:40:10 -0700

    Add pumpkin goblin

---
+++ src/common/Items/mats/misc/Pumpkin.lua
@@ -0,0 +1,12 @@
+return {
+    id = "pumpkin",
+    name = "Pumpkin",
+    tier = 1,
+    spriteSheet = "materials",
+    spriteCoords = Vector2.new(2,4),
+
+    tags = {
+        "material",
+        "event",
+    }
+}
---
+++ src/common/NPCS/special/pumpkinGoblin.lua
@@ -0,0 +1,30 @@
+return {
+    npcType = script.Name,
+    name = "Goblin",
+    propsGenerator = function()
+        return {
+            ActorStats = {
+                maxHealth = 10,
+                health = 10,
+                moveSpeed = 16,
+                baseDamage = 10
+            },
+            ItemDrops = {
+                items = {
+                    {itemId = "pumpkin", dropRange = {min = 1, max = 1}, dropRate = 1},
+                },
+                cash = 200,
+            }
+        }
+    end,
+    boundingBoxProps = {
+        Size = Vector3.new(2,4.5,2),
+        Color = Color3.fromRGB(255, 123, 0),
+    },
+
+    animations = {
+        attack = "r6attack",
+        chase = "r6run",
+        idle = "r6idle",
+    },
+}
