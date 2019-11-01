b91401fbd8eea0c437be868d669ef8b33a074498
Nimblz <areuschle1@gmail.com>
2019-11-01 16:39:49 -0700

    Add new cheats

--- misc/cheats.lua
+++ misc/cheats.lua
@@ -10,3 +10,18 @@ while _G.killing do
     end
     wait()
 end
+
+-- WIPE SAVE --
+
+local playerName = "Nimblz"
+local player = game.Players:FindFirstChild(playerName)
+local action = {type = "PLAYER_ADD", player = player, saveData = {}, replicateTo = player}
+_G.store:dispatch(action)
+
+-- ADD ITEM --
+
+local playerName = "Nimblz"
+local player = game.Players:FindFirstChild(playerName)
+local action = {type = "ITEM_ADD", player = player, itemId = "wood", quantity = 10, replicateTo = player}
+_G.store:dispatch(action)
+
