279a73186260c28deaf2d7b828d32d652a38120b
Nimblz <areuschle1@gmail.com>
2019-11-01 15:02:39 -0700

    Beautify cash

--- src/client/gameModules/GameGui/uiComponents/MenuBar/CashLabel.lua
+++ src/client/gameModules/GameGui/uiComponents/MenuBar/CashLabel.lua
@@ -4,6 +4,7 @@ local LocalPlayer = Players.LocalPlayer
 
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
+local util = common:WaitForChild("util")
 local component = script:FindFirstAncestor("uiComponents")
 
 local Roact = require(lib:WaitForChild("Roact"))
@@ -11,7 +12,11 @@ local RoactRodux = require(lib:WaitForChild("RoactRodux"))
 local Selectors = require(common:WaitForChild("Selectors"))
 
 local RoundFrame = require(component:WaitForChild("RoundFrame"))
+local FitList = require(component:WaitForChild("FitList"))
+local FitText = require(component:WaitForChild("FitText"))
 local CashLabel = Roact.Component:extend("CashLabel")
+
+local beautifyNumber = require(util:WaitForChild("beautifyNumber"))
 
 local HORIZONTAL_SIZE = 200
 local VERTICAL_SIZE = 72
@@ -28,24 +33,14 @@ function CashLabel:render()
     local cashValue = self.props.cashValue or 0
 
     local children = {
-        listLayout = Roact.createElement("UIListLayout", {
-            SortOrder = Enum.SortOrder.LayoutOrder,
-            VerticalAlignment = Enum.VerticalAlignment.Center,
-            HorizontalAlignment = Enum.HorizontalAlignment.Left,
-            Padding = UDim.new(0,PADDING),
-            FillDirection = Enum.FillDirection.Horizontal,
-        }),
-        padding = Roact.createElement("UIPadding", {
-            PaddingLeft = UDim.new(0,12)
-        }),
         icon = Roact.createElement("ImageLabel", {
             Image = "rbxassetid://3977083793",
             Size = UDim2.new(0,48,0,48),
             BackgroundTransparency = 1,
             LayoutOrder = 1,
         }),
-        coinText = Roact.createElement("TextLabel", {
-            Text = (cashValue and "$"..tostring(math.floor(cashValue))) or "N/A",
+        coinText = Roact.createElement(FitText, {
+            Text = (cashValue and "$"..beautifyNumber(cashValue)) or "N/A",
             Size = UDim2.new(0,0,1,0),
             BackgroundTransparency = 1,
             TextColor3 = Color3.new(1,1,1),
@@ -57,11 +52,29 @@ function CashLabel:render()
         })
     }
 
-    return Roact.createElement(RoundFrame, {
-        Size = UDim2.new(0,HORIZONTAL_SIZE,0,VERTICAL_SIZE),
-        Position = UDim2.new(0,PADDING,1,-PADDING),
-        AnchorPoint = Vector2.new(0,1),
-        LayoutOrder = 1,
+    return Roact.createElement(FitList, {
+        containerKind = RoundFrame,
+        containerProps = {
+            Size = UDim2.new(0,HORIZONTAL_SIZE,0,VERTICAL_SIZE),
+            Position = UDim2.new(0,PADDING,1,-PADDING),
+            AnchorPoint = Vector2.new(0,1),
+            LayoutOrder = 1,
+        },
+        fitAxes = "X",
+        minSize = Vector2.new(HORIZONTAL_SIZE,0),
+        layoutProps = {
+            SortOrder = Enum.SortOrder.LayoutOrder,
+            VerticalAlignment = Enum.VerticalAlignment.Center,
+            HorizontalAlignment = Enum.HorizontalAlignment.Left,
+            Padding = UDim.new(0,PADDING),
+            FillDirection = Enum.FillDirection.Horizontal,
+        },
+        paddingProps = {
+            PaddingTop = UDim.new(0,12),
+            PaddingBottom = UDim.new(0,12),
+            PaddingLeft = UDim.new(0,12),
+            PaddingRight = UDim.new(0,12),
+        }
     }, children)
 end
 
