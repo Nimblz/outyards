e9b2eaf2ddf9e6a91b03271dca56e0ceef2ce7d1
Nimblz <areuschle1@gmail.com>
2019-11-05 14:10:13 -0800

    Add scaling to views

--- src/client/gameModules/GameGui/uiComponents/views/makeView.lua
+++ src/client/gameModules/GameGui/uiComponents/views/makeView.lua
@@ -3,15 +3,25 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
 local util = common:WaitForChild("util")
+local component = script:FindFirstAncestor("uiComponents")
 
 local Dictionary = require(util:WaitForChild("Dictionary"))
 local Selectors = require(common:WaitForChild("Selectors"))
 local Roact = require(lib:WaitForChild("Roact"))
 local RoactRodux = require(lib:WaitForChild("RoactRodux"))
+local withScale = require(component:WaitForChild("withScale"))
 
 local function makeView(component, viewId)
     -- wraps component in a new component that is only visible when its viewid is visible
     local newView = Roact.Component:extend("view_"..viewId)
+
+    local componentWithScale = withScale(component, {
+        defaultSize = Vector2.new(1280,800),
+        scale = 1,
+        minScale = 0.5,
+        maxScale = 1,
+        scaleIncrement = 0.5,
+    })
 
     function newView:render()
 
@@ -28,7 +38,7 @@ local function makeView(component, viewId)
             ZIndex = 2,
             Visible = self.props.visible,
         }, {
-            view = Roact.createElement(component, prunedProps)
+            view = Roact.createElement(componentWithScale, prunedProps)
         })
     end
 
