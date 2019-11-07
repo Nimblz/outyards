995a8f24972b91173fab3f4aa4afc0d93d2cb7d2
Nimblz <areuschle1@gmail.com>
2019-11-06 19:25:34 -0800

    Block out crafting ui

--- src/client/gameModules/GameGui/uiComponents/views/Crafting/init.lua
+++ src/client/gameModules/GameGui/uiComponents/views/Crafting/init.lua
@@ -3,41 +3,86 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local common = ReplicatedStorage:WaitForChild("common")
 local lib = ReplicatedStorage:WaitForChild("lib")
 local event = ReplicatedStorage:WaitForChild("event")
-local components = script:FindFirstAncestor("uiComponents")
+local util = common:WaitForChild("util")
+local component = script:FindFirstAncestor("uiComponents")
 
-local Selectors = require(common:WaitForChild("Selectors"))
+local eRequestEquip = event:WaitForChild("eRequestEquip")
+
+local Dictionary = require(util:WaitForChild("Dictionary"))
 local Roact = require(lib:WaitForChild("Roact"))
-local RoactRodux = require(lib:WaitForChild("RoactRodux"))
 
-local RoundFrame = require(components:WaitForChild("RoundFrame"))
-local Crafting = Roact.Component:extend("Crafting")
+local RoundFrame = require(component:WaitForChild("RoundFrame"))
+local FitList = require(component:WaitForChild("FitList"))
+local CraftingBody = require(script:WaitForChild("CraftingBody"))
+local CraftingNavbar = require(script:WaitForChild("CraftingNavbar"))
 
 local makeView = require(script.Parent:WaitForChild("makeView"))
 
+local Crafting = Roact.PureComponent:extend("Crafting")
+
 function Crafting:init()
+    self:setState({
+        tagFilter = "all",
+        searchFilter = "",
+    })
 end
 
-function Crafting:didMount()
+function Crafting:setTagFilter(newTag)
+    self:setState({
+        tagFilter = newTag,
+    })
+end
+
+function Crafting:setSearchFilter(newSearch)
+    self:setState({
+        searchFilter = newSearch,
+    })
 end
 
 function Crafting:render()
-    local visible = self.props.visible
 
-    return Roact.createElement(RoundFrame, {
-        ImageTransparency = 0,
-        Position = UDim2.new(0.5,0,0.5,0),
-        Size = UDim2.new(0,600,0,400),
-        AnchorPoint = Vector2.new(0.5,0.5),
-        Visible = visible
-    }, {
-        Roact.createElement("TextLabel", {
-            Text = "WIP LOL",
+    local viewContent = {
+        title = Roact.createElement("TextLabel", {
+            Text = "Crafting",
             Font = Enum.Font.GothamBlack,
-            Size = UDim2.new(1,0,1,0),
-            TextSize = 64,
+            Size = UDim2.new(1,0,0,32),
+            TextSize = 32,
             BackgroundTransparency = 1,
+            TextXAlignment = Enum.TextXAlignment.Left,
+
+            LayoutOrder = 1,
+        }),
+        navbar = Roact.createElement(CraftingNavbar, {
+            searchUpdate = function(newText) self:setSearchFilter(newText) end,
+            tagUpdate = function(newTag) self:setTagFilter(newTag) end,
+        }),
+        body = Roact.createElement(CraftingBody, {
+            searchFilter = self.state.searchFilter,
+            tagFilter = self.state.tagFilter,
         })
-    })
+    }
+
+    local children = Dictionary.join(viewContent, self.props[Roact.Children])
+
+    return Roact.createElement(FitList, {
+        containerKind = RoundFrame,
+        scale = 1,
+        layoutProps = {
+            Padding = UDim.new(0,16)
+        },
+        paddingProps = {
+            PaddingTop = UDim.new(0,16),
+            PaddingBottom = UDim.new(0,16),
+            PaddingLeft = UDim.new(0,16),
+            PaddingRight = UDim.new(0,16),
+        },
+        containerProps = {
+            Position = UDim2.new(0.5,0,0.5,0),
+            AnchorPoint = Vector2.new(0.5,0.5),
+            ZIndex = 2,
+            Active = true,
+        }
+    }, children)
 end
 
 Crafting = makeView(Crafting, "crafting")
