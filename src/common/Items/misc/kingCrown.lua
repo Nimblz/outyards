107ba6170ea0c4c4ff187abfb4acfcd6efd5a419
Nimblz <areuschle1@gmail.com>
2019-11-18 11:02:57 -0800

    Add king crown

---
+++ src/common/Items/misc/kingCrown.lua
@@ -0,0 +1,12 @@
+return {
+    id = "kingCrown",
+    name = "Ogre King's Cown",
+    desc = "Ancient crown worn by the ogre king.",
+    tier = 99,
+    spriteSheet = "keyItem",
+    spriteCoords = Vector2.new(3,2),
+
+    tags = {
+        "keyItem",
+    },
+}
