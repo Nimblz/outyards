1d801de04dbefdd09ef8fd8ae18a4a91e5fcd6f5
Nimblz <areuschle1@gmail.com>
2019-11-18 10:54:45 -0800

    Add armored ogre

---
+++ src/common/NPCS/grass/armoredOgre.lua
@@ -0,0 +1,30 @@
+return {
+    npcType = script.Name,
+    name = "Armored Ogre",
+    propsGenerator = function()
+        return {
+            ActorStats = {
+                maxHealth = 75,
+                health = 75,
+                moveSpeed = 18,
+            },
+            ItemDrops = {
+                items = {
+                    {itemId = "oreIron", dropRange = {min = 1, max = 3}, dropRate = 0.75},
+                    {itemId = "oreGold", dropRange = {min = 1, max = 3}, dropRate = 0.2},
+                },
+                cash = 10,
+            }
+        }
+    end,
+    boundingBoxProps = {
+        Size = Vector3.new(4,12,4),
+        Color = Color3.fromRGB(105, 141, 88),
+    },
+
+    animations = {
+        attack = "r6attack",
+        chase = "r6run",
+        idle = "r6idle",
+    },
+}
