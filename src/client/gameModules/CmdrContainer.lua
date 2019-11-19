dcdec8da77b65bf84895963cf7573e9a00eca15e
Nimblz <areuschle1@gmail.com>
2019-11-19 11:15:42 -0800

    Add logging

--- src/client/gameModules/CmdrContainer.lua
+++ src/client/gameModules/CmdrContainer.lua
@@ -1,24 +1,33 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lib = ReplicatedStorage.lib
+local cmdrModule = ReplicatedStorage:WaitForChild("CmdrClient")
+cmdrModule:WaitForChild("Commands")
 
 local PizzaAlpaca = require(lib.PizzaAlpaca)
-local Cmdr = require(ReplicatedStorage:WaitForChild("CmdrClient"))
+local Cmdr = require(cmdrModule)
 
 
 local CmdrContainer = PizzaAlpaca.GameModule:extend("CmdrContainer")
 
 function CmdrContainer:onStore(store)
+    self.logger:log("Starting Cmdr...")
+
+    self.logger:log("Registering Hooks...")
     Cmdr.Registry:RegisterHook("BeforeRun", function(context)
         context.State.pzCore = self.core
         context.State.store = store
     end)
 
+    self.logger:log("Setting activation keys...")
     Cmdr:SetActivationKeys({ Enum.KeyCode.RightAlt })
+
+    self.logger:log("Cmdr Started!")
 end
 
 function CmdrContainer:init()
     local storeContainer = self.core:getModule("StoreContainer")
+    self.logger = self.core:getModule("Logger"):createLogger(self)
 
     storeContainer:getStore():andThen(function(store)
         self:onStore(store)
