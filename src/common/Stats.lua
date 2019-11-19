41ab467c084573319067c4276d5ccc77b6a1ec45
Nimblz <areuschle1@gmail.com>
2019-11-19 14:05:08 -0800

    Add remaining stats

--- src/common/Stats.lua
+++ src/common/Stats.lua
@@ -7,17 +7,46 @@ local by = require(util.by)
 
 local stats = {
     {
+        id = "damageType",
+        name = "Damage Type",
+        desc = "Type of damage this weapon deals."
+    },
+    {
         id = "baseDamage",
-        name = "Damage"
+        name = "Damage",
+        desc = "Base amount of damage dealt to monsters."
+    },
+    {
+        id = "armor",
+        name = "Armor"
     },
     {
         id = "defense",
-        name = "Damage"
+        name = "Defense"
     },
     {
-        id = "defense",
-        name = "Damage"
+        id = "attackRate",
+        name = "Attack Speed"
     },
+    {
+        id = "meleeModifier",
+        name = "Melee Damage",
+        desc = "Melee damage multiplier",
+    },
+    {
+        id = "rangedModifier",
+        name = "Ranged Damage",
+        desc = "Ranged damage multiplier",
+    },
+    {
+        id = "magicModifier",
+        name = "Magic Damage",
+        desc = "Magic damage multiplier",
+    },
+    {
+        id = "moveSpeed",
+        name = "Move Speed"
+    }
 }
 
 return {
